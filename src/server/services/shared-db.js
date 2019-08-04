1eba5280e807c8359d4f302e60808e0be4107a50
Aldon <aisenberg@freightview.com>
2019-08-04 12:11:59 -0500

    remove colors temporarily

--- src/server/services/shared-db.js
+++ src/server/services/shared-db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const colors = require('colors')
+// const colors = require('colors') * need to figure out a way to handle keeping this a dev-dependency by dev vs production console logging
 
 let handleFailure = function() {
   console.log("There was an error".bold.red.bgWhite);
@@ -13,7 +13,7 @@ module.exports = {
 
     mongoose.connection.on("error", handleFailure)
     mongoose.connection.on("open", function() {
-      console.log(`MongoDB connected to[${connection.db.databaseName}]`.grey.bgRed)
+      console.log(`MongoDB connected to[-- ${connection.db.databaseName} --]`)//.grey.bgRed)
     })
     mongoose.connect(process.env.MONGO_CLIENT_WRESTLEDB, { useNewUrlParser : true } )
   }
