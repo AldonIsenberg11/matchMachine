17650381644be8a073fb54771c17ba9a9fbe7871
Aldon <aisenberg@freightview.com>
2020-01-12 16:32:03 -0600

    include virtuals on Match Model

--- src/server/models/matchSchema.js
+++ src/server/models/matchSchema.js
@@ -1,21 +1,33 @@
+const _ = require('lodash')
 const mongoose = require('mongoose')
 
-let Schema = mongoose.Schema
+const opts = { toJSON: { virtuals: true } }
 
-let matchSchema = new Schema({
+const matchSchema = mongoose.Schema({
   matchRules: Object, // rulebookVersion => style: "folkstyle", matchLength: "", periods: ""
-  eventId: Schema.Types.ObjectId,
+  eventId: mongoose.Schema.Types.ObjectId,
   completed: Boolean,
   boutNo: Number,
   startTime: Date,
   referee: Object,
   wrestler1: String,
   wrestler2: String,
-  // actions: [matchAction],
-  events: {type: [Schema.Types.Mixed], default: []},
+  events: {type: [mongoose.Schema.Types.Mixed], default: []},
   matchStatus: String, // started, live, paused, complete
   winner: Object,
-  victoryType: String, //decision, major decision, pin, techfall, forfeit, disqualification.
+  victoryType: String //decision, major decision, pin, techfall, forfeit, disqualification.
+}, opts)
+
+matchSchema.virtual('wrestler1Score').get(function () {
+  return _.sum(this.events
+    .filter((event) => (event.type === "scoring" && event.result.wrestlerAwarded === 'wrestler1'))
+    .map((event) => { return (event.result.pointsAwarded || 0) }))
+})
+
+matchSchema.virtual('wrestler2Score').get(function () {
+  return _.sum(this.events
+    .filter((event) => (event.type === "scoring" && event.result.wrestlerAwarded === 'wrestler2'))
+    .map((event) => { return (event.result.pointsAwarded || 0) }))
 })
 
 const Match = mongoose.model('Match', matchSchema)
